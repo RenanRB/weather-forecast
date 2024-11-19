26abbfa0dde066bd328e3188420bd0647fc40161
test <renan10r_@hotmail.com>
2024-11-19 00:59:00 -0300

    docker

--- back/src/server.ts
+++ back/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'http://localhost:8080'],
     credentials: true
 }));
 
